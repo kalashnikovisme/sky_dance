274bd23610524e7ab87b67f4a31c2bc6720f9068
kalashnikovisme <kalashnikovisme@gmail.com>
2013-05-31 01:10:21 +0400

    add admin

---
+++ lib/auth_helper.rb
@@ -0,0 +1,13 @@
+module AuthHelper
+  def admin_sign_in(admin)
+    session[:admin_id] = admin.id
+  end
+
+  def admin_signed_in?
+    session[:admin_id] && Admin.find_by_id(session[:admin_id])
+  end
+
+  def admin_sign_out
+    session[:admin_id] = nil
+  end
+end

2d4605cf5435a444fc6d861b8a49d53c726f2f0f
kalashnikovisme <kalashnikovisme@gmail.com>
2013-05-31 04:16:58 +0400

    add edit and update action to teacher

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -23,4 +23,26 @@ class TeachersController < ApplicationController
   def index
     @teachers = Teacher.all
   end
+
+  def edit
+    if admin_signed_in?
+      @teacher = Teacher.find params[:id]
+    else
+      redirect_to '/404'
+    end
+  end
+
+  def update
+    if admin_signed_in?
+      @teacher = Teacher.find params[:id]
+
+      if @teacher.update_attributes params[:teacher]
+        redirect_to @teacher
+      else
+        render action: "edit"
+      end
+    else
+      redirect_to '/404'
+    end
+  end
 end
--- test/functional/teacher_controller_test.rb
+++ test/functional/teacher_controller_test.rb
@@ -40,4 +40,33 @@ class TeachersControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+
+  test "should get edit" do
+    admin_sign_in @admin  
+
+    get :edit, id: @teacher
+    assert_response :success
+  end
+
+  test "should not get edit with no access" do
+    get :edit, id: @teacher
+    assert_redirected_to '/404'
+  end
+
+  test "should update teacher" do
+    admin_sign_in @admin
+ 
+    attributes = attributes_for :teacher
+    put :update, id: @teacher, teacher: attributes
+    assert_response :redirect
+
+    @teacher.reload
+    assert_equal attributes[:first_name], @teacher.first_name
+  end
+
+  test "should not update teacher with no access" do
+    attributes = attributes_for :teacher
+    put :update, id: @teacher, teacher: attributes
+    assert_redirected_to '/404'
+  end
 end

9293b760f832b771b2f774db9542b2e4b1d48684
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-05 02:02:42 +0400

    some formatting

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,8 @@
 class Teacher < ActiveRecord::Base
-  attr_accessible :description, :first_name, :middle_name, :motto
+  attr_accessible :description,
+                  :first_name,
+                  :middle_name,
+                  :motto
 
   has_many :teacher_photos, :dependent => :destroy
 
--- app/models/teacher_photo.rb
+++ app/models/teacher_photo.rb
@@ -1,7 +1,10 @@
 class TeacherPhoto < ActiveRecord::Base
-  attr_accessible :teacher_id, :photo
+  attr_accessible :teacher_id,
+                  :photo
 
   belongs_to :teacher
   has_attached_file :photo
+
   validates :photo, presence: true
+  validates :teacher_id, presence: true
 end

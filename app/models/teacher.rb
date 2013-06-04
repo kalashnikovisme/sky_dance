4d988a623fa8767969a341776ca68344becb3b3c
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-05 01:57:12 +0400

    add fio to teachers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,9 @@
 class Teacher < ActiveRecord::Base
   attr_accessible :description, :first_name, :middle_name, :motto
-    
+
   has_many :teacher_photos, :dependent => :destroy
+
+  def fio
+    "#{first_name} #{middle_name}"
+  end
 end

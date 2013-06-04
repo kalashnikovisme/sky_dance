94303d6572445ab8dc7bcb817cf5fdabf9c2433f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-05 01:59:54 +0400

    add validates to teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,6 +3,12 @@ class Teacher < ActiveRecord::Base
 
   has_many :teacher_photos, :dependent => :destroy
 
+  validates :first_name, presence: true
+  validates :middle_name, presence: true
+  validates :description, presence: true
+  validates :motto, presence: true,
+                    length: { maximum: 140 }
+
   def fio
     "#{first_name} #{middle_name}"
   end

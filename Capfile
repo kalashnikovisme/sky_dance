e3087b3bb312665e15e8083628e0713d19f180f6
aelaa <aelaau@gmail.com>
2013-06-06 01:20:44 +0400

    capistrano added

---
+++ Capfile
@@ -0,0 +1,3 @@
+load 'deploy'
+require 'capi/unicorn'
+load 'config/deploy' # remove this line to skip loading any of the default tasks
---
+++ config/deploy.rb
@@ -0,0 +1,52 @@
+require 'capi/unicorn'
+require "rvm/capistrano"
+
+set :application, "skydance"
+set :rails_env,   "production"
+
+set :rvm_type, :user
+
+role :web, "skydance73.ru"
+role :db, "skydance73.ru", :primary => true
+
+set :user, "user"  # The server's user for deploys
+
+set :use_sudo, false
+
+set :rake, "#{rake} --trace"
+set :app_dir, "/srv/#{application}"
+  set :current_path, "#{app_dir}/current"
+set :deploy_to, "#{app_dir}"
+
+set :ssh_options, { :forward_agent => true }
+default_run_options[:pty] = true
+
+set :repository, "https://github.com/kalashnikovisme/sky_dance.git"  # Your clone URL
+set :scm, "git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+desc "Seed database data"
+task :seed_data do
+  run "cd #{current_path} && RAILS_ENV=#{rails_env} #{rake} db:seed"
+end
+
+namespace :db do
+  desc "Symlink to sqlite db"
+    task :symlink do
+        run "cd #{current_path}/db && ln -s #{app_dir}/shared/db/development.sqlite3"
+          end
+end
+
+namespace :bundler do
+  task :install do
+      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle install"
+        end
+end
+
+after 'deploy:create_symlink', 'db:symlink'
+after 'deploy:finalize_update', 'bundler:install'
+after 'deploy:migrate', 'bundler:install'
+
+require 'capistrano_colors'
+

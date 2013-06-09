cf0f0a6c221d61c4b5114c9749b3e2712e9de390
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-10 01:18:20 +0400

    edit photos factory

--- test/factories/photos.rb
+++ test/factories/photos.rb
@@ -1,9 +1,4 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :photo do
-    description "MyText"
-    file "MyString"
-    attachable nil
   end
 end

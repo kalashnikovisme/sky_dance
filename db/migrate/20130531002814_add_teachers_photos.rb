5fbb8e8071bdeb759ddf9e0eaf7537a25096eb13
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-09 22:03:53 +0400

    remove teacher_photos

--- db/migrate/20130531002814_add_teachers_photos.rb
+++
@@ -1,9 +0,0 @@
-class AddTeachersPhotos < ActiveRecord::Migration
-  def change
-    create_table :teacher_photos do |t|
-      t.attachment :photo
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130531003231_add_teacher_id_to_teacher_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddTeacherIdToTeacherPhotos < ActiveRecord::Migration
-  def change
-    add_column :teacher_photos, :teacher_id, :integer
-  end
-end

66040440691bd7eae5370e2f61276b075744e8cb
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-12 04:43:06 +0400

    add good genres migration

--- db/schema.rb
+++ db/schema.rb
@@ -27,14 +27,9 @@ ActiveRecord::Schema.define(:version => 20130611234400) do
   end
 
   create_table "genres", :force => true do |t|
-    t.string   "title"
-    t.string   "description"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.string "description"
+    t.string "photo"
+    t.string "title"
   end
 
   create_table "groups", :force => true do |t|
@@ -66,16 +61,6 @@ ActiveRecord::Schema.define(:version => 20130611234400) do
   end
 
   add_index "photos", ["attachable_id"], :name => "index_photos_on_attachable_id"
-
-  create_table "teacher_photos", :force => true do |t|
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.integer  "teacher_id"
-  end
 
   create_table "teachers", :force => true do |t|
     t.string   "first_name"

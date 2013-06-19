6426dd61b958fd686fab10c89dcb2bebb4885da1
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-19 04:49:19 +0400

    add about and contacts pages

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
+  setup do
+    @video = create :video
+  end
 
   test "should get index" do
     get :index
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contacts" do
+    get :contacts
     assert_response :success
   end
 

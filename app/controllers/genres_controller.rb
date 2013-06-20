fe69bc5adb3a684206a0f6d1d53c5069bf393a58
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-20 15:45:02 +0400

    add genres edit form

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -24,4 +24,36 @@ class GenresController < ApplicationController
     @genre = Genre.find params[:id]
     set_viewed_genre @genre
   end
+
+  def edit
+    if admin_signed_in?
+      @genre = Genre.find params[:id]
+    else
+      redirect_to '/404'
+    end
+  end
+
+  def update
+    if admin_signed_in?
+      @genre = Genre.find params[:id]
+
+      if @genre.update_attributes params[:genre]
+        redirect_to @genre
+      else
+        render action: 'edit'
+      end
+    else
+      redirect_to '/404'
+    end
+  end
+
+  def destroy
+    if admin_signed_in?
+      @genre = Genre.find params[:id]
+      @genre.destroy
+      redirect_to genres_url
+    else
+      redirect_to '/404'
+    end
+  end
 end

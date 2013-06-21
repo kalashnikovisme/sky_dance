6804211db385aab50602be755b15db770ab84249
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-22 01:18:58 +0400

    fix bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,11 @@ class WelcomeController < ApplicationController
   def index
     @genres = Genre.all
     #@news = News.first 3
-    @video = Video.last.decorate
     @photos_bottom = Photo.last 3
     @teachers = TeacherDecorator.decorate_collection Teacher.all.shuffle!
+    if Video.any?
+      @video = Video.last.decorate
+    end
   end
 
   def contacts

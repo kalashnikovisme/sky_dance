1bbc7c7ff4c8060e7c4891e8b0be9699e34997bc
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-23 01:59:28 +0400

    remove bad functions

--- lib/auth_helper.rb
+++ lib/auth_helper.rb
@@ -14,20 +14,4 @@ module AuthHelper
   def authenticate_admin?(admin, password)
     admin.password === password
   end
-
-  def set_viewed_teacher(viewed_teacher)
-    session[:viewed_teacher_id] = viewed_teacher.id
-  end
-
-  def viewed_teacher
-    viewed_teacher = Teacher.find(session[:viewed_teacher_id])
-  end
-
-  def set_viewed_genre(viewed_genre)
-    session[:viewed_genre_id] = viewed_genre.id
-  end
-
-  def viewed_genre
-    viewed_genre = Genre.find(session[:viewed_genre_id])
-  end
 end

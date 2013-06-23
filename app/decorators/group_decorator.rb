3b401f55cea306b4d77fab5638f571e72f0107bc
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-23 04:26:20 +0400

    add enumerize locales

--- app/decorators/group_decorator.rb
+++ app/decorators/group_decorator.rb
@@ -11,15 +11,4 @@ class GroupDecorator < Draper::Decorator
   def find_lesson_by_day(day)
     model.lessons.find_by_day(day)
   end
-
-  #FIXME
-  def space_locale
-    if model.space === "there_places"
-      space_local = "есть места в группе"
-    end
-    if model.space === "there_not_places"
-      space_local = "мест в группе нет"
-    end
-    space_local = "группа формируется"
-  end
 end

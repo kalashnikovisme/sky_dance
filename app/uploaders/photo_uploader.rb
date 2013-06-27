316a77a0e85e3b8ff8b79c103d5060c6245c6ca4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-27 04:24:30 +0400

    edit uploadr

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -8,8 +8,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fill: [256, 256]
-
   version :thumb do
     process :resize_to_fill => [100,100]
   end

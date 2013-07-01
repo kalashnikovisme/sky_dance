e0594e8173b16b1a072bbb804cca75ab3a933a45
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-07-01 14:58:19 +0400

    add errors test

--- test/functional/errors_controller_test.rb
+++ test/functional/errors_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ErrorsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get not_found" do
+    get :not_found
+    assert_response 404
+  end
 end

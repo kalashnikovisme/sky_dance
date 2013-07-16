efa0f5aabcf5fac3915f2a4f67bc2fdb4079af1d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-07-16 04:12:55 +0400

    good print

--- app/reports/schedule_report.rb
+++ app/reports/schedule_report.rb
@@ -6,28 +6,40 @@ class ScheduleReport < Prawn::Document
         :italic => "/home/pavel/prawn_fonts/verdanai.ttf",
         :normal  => "/home/pavel/prawn_fonts/verdana.ttf" })
     font "Verdana", :size => 10
-    text "Отчет за #{Time.zone.now.strftime('%b %Y')}", :size => 15, :style => :bold, :align => :center
+    text "Расписание школы танцев Sky Dance", size: 22, style: :bold, align: :center
     move_down(18)
     @genres = Genre.all
     data = []
 
     @genres.each do |genre|
+      font_size 20
+      image genre.photo.thumb, width: 35
+      move_down 5
       text genre.title
+      move_down 20
       genre.groups.decorate.map do |group|
-        text "#{group.teacher.decorate.fio} #{group.category.describe}"
-        data << ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
+        image group.teacher.decorate.first_photo_thumb, width: 25
+        move_down 5
+        font_size 16
+        text "#{group.teacher.decorate.fio} - #{group.category.describe}"
+        move_down 10
+        font_size 10
+        data << ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс", "Цена за курс", "за одно занятие"]
+        font_size 12
         data << [group.lesson_time(:monday),
                  group.lesson_time(:tuesday),
                  group.lesson_time(:wednesday),
                  group.lesson_time(:thursday),
                  group.lesson_time(:friday),
                  group.lesson_time(:saturday),
-                 group.lesson_time(:sunday)]
+                 group.lesson_time(:sunday),
+                 "#{group.price} руб.",
+                 "#{group.once_price} руб."]
       end
       table data, width: 500
     end
 
-    creation_date = Time.zone.now.strftime("Отчет сгенерирован %e %b %Y в %H:%M")
+    creation_date = Time.zone.now.strftime("Расписание актуально на  %e %b %Y в %H:%M")
     go_to_page(page_count)
     move_down(710)
     text creation_date, :align => :right, :style => :italic, :size => 9

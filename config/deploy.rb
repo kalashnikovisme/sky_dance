0cabb18492905ecd97b5ecfa41a131d2927745f3
aelaa <aelaau@gmail.com>
2013-07-23 21:53:01 +0400

    cap recipes rebuilded

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,45 +21,64 @@ set :deploy_to, "#{app_dir}"
 set :ssh_options, { :forward_agent => true }
 default_run_options[:pty] = true
 
+timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
+
 set :repository, "https://github.com/kalashnikovisme/sky_dance.git"  # Your clone URL
 set :scm, "git"
 set :branch, "master"
 set :deploy_via, :remote_cache
 
-desc "Seed database data"
-task :seed_data do
-  run "cd #{current_path} && RAILS_ENV=#{rails_env} #{rake} db:seed"
+
+namespace :files do
+	desc "Files backup"
+	task :backup do
+	  run "mkdir #{app_dir}/shared/backup/uploads/#{timestamp} && cp -rf #{app_dir}/shared/uploads/* #{app_dir}/shared/backup/uploads/#{timestamp}/ && rm #{app_dir}/shared/uploads && ln -sf #{app_dir}/shared/backup/uploads/#{timestamp} #{app_dir}/shared/uploads"
+	end
+	
+	desc "Restore link to uploads"
+	task :link_update do
+		run "rm -rf #{current_path}/public/uploads && cp -Pf #{app_dir}/shared/uploads #{current_path}/public/uploads"
+	end
 end
 
-desc "Photos caching"
-task :save_photos do
-  run "mv #{app_dir}/shared/uploads/uploads #{app_dir}/shared/uploads/#{Time.now.utc.strftime("%Y%m%d%H%M%S")} && cd #{current_path}/public && cp -r uploads/ #{app_dir}/shared/uploads"
-end
-
-desc "Photos restoring"
-task :restore_photos do
-  run "cd #{current_path}/public && cp -r #{app_dir}/shared/uploads/uploads ./"
-end
 
 namespace :db do
-  desc "Symlink to sqlite db"
-    task :symlink do
-        run "cd #{current_path}/db && ln -s #{app_dir}/shared/db/development.sqlite3"
-          end
+  desc "Backup database"
+  task :backup do
+  	run "pg_dump skydance >  #{app_dir}/shared/backup/db/#{timestamp}_dump.sql"
+ 	end
+
+	desc "Seed database data"
+		task :seed do
+		  run "cd #{current_path} && RAILS_ENV=#{rails_env} #{rake} db:seed"
+	end
 end
 
 namespace :bundler do
+  desc "Run 'bundle install'"
   task :install do
-      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle install"
-        end
+ 		run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle install"
+  end
 end
 
-before 'deploy:update', 'save_photos'
-before 'deploy:update', 'unicorn:stop'
-after 'deploy:update', 'restore_photos'
-after 'deploy:update', 'unicorn:start'
-after 'deploy:create_symlink', 'db:symlink'
+namespace :assets do
+	desc "Run 'rake assets:clean'"
+	task :clean do
+		run "cd #{current_path} && RAILS_ENV=#{rails_env} rake assets:clean"
+	end
+
+	desc "Run 'rake assets:precompile'"
+	task :precompile do
+		run "cd #{current_path} && RAILS_ENV=#{rails_env} rake assets:precompile"
+	end
+
+end
+
+before 'deploy:update', 'unicorn:stop', "files:backup", "db:backup"
 after 'deploy:finalize_update', 'bundler:install'
+after 'deploy:update', 'assets:precompile', 'files:link_update', 'unicorn:start'
+
+before 'assets:precompile', 'assets:clean'
 after 'deploy:migrate', 'bundler:install'
 
 require 'capistrano_colors'

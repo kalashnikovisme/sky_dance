f1bcfe94cf75026c320eff65002cef0ec1d6d172
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-07-29 14:03:31 +0400

    remove brackets from schedule_report

--- app/reports/schedule_report.rb
+++ app/reports/schedule_report.rb
@@ -2,26 +2,26 @@ class ScheduleReport < Prawn::Document
   def font_style
     font_families.update(
       "Verdana" => {
-        :bold => "#{Rails.root}/public/assets/verdana-bold.ttf",
-        :italic => "#{Rails.root}/public/assets/verdana-italic.ttf",
-        :normal  => "#{Rails.root}/public/assets/verdana.ttf"
+        bold:"#{Rails.root}/public/assets/verdana-bold.ttf",
+        italic: "#{Rails.root}/public/assets/verdana-italic.ttf",
+        normal: "#{Rails.root}/public/assets/verdana.ttf"
         })
-    font "Verdana", :size => 10
+    font "Verdana", size: 10
   end
 
   def header
-    text I18n.t('reports.head'), size: 22, style: :bold, align: :center
-    move_down(18)
+    text I18n.t 'reports.head', size: 22, style: :bold, align: :center
+    move_down 18
   end
 
   def footer
-    creation_date = Time.zone.now.strftime(I18n.t('report.actual_for'))
-    go_to_page(page_count)
-    move_down(710)
-    text creation_date, :align => :right, :style => :italic, :size => 9
+    creation_date = Time.zone.now.strftime I18n.t 'report.actual_for'
+    go_to_page page_count
+    move_down 710
+    text creation_date, align: :right, style: :italic, size: 9
   end
 
-  def group_row
+  def group_row(group)
     image group.teacher.decorate.first_photo_thumb, width: 25
     move_down 5
     font_size 16
@@ -40,18 +40,18 @@ class ScheduleReport < Prawn::Document
              group.lesson_time(:friday),
              group.lesson_time(:saturday),
              group.lesson_time(:sunday),
-             "#{group.price} #{I18n.t('rub')}",
-             "#{group.once_price} #{I18n.t('rub')}"]
+             "#{group.price} #{I18n.t 'rub'}",
+             "#{group.once_price} #{I18n.t 'rub'}"]
   end
 
-  def genre_table
+  def genre_table(genre)
     font_size 20
     image genre.photo.thumb, width: 35
     move_down 5
     text genre.title
     move_down 20
     genre.groups.decorate.map do |group|
-      group_row
+      group_row group
     end
     table data, width: 500
   end
@@ -63,7 +63,7 @@ class ScheduleReport < Prawn::Document
     data = []
 
     @genres.each do |genre|
-      genre_table
+      genre_table genre
     end
 
     footer

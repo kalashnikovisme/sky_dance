e275f41d269cea908484cac1b41b546cef062e6c
pavel <kalashnikovisme@gmail.com>
2013-09-27 05:42:38 +0400

    remigrate

--- db/schema.rb
+++ db/schema.rb
@@ -101,7 +101,6 @@ ActiveRecord::Schema.define(:version => 20130920194121) do
     t.integer  "price"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.text     "title"
     t.text     "name"
   end
 

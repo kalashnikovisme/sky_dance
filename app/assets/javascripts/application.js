471fc092c52bcffa12a694c5a0f65a522a79c7c1
OlegZotov <olegzotov73@gmail.com>
2013-09-30 02:13:40 +0400

    Increasing the image size onclick on page /welcome/contacts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require fancybox
 //= require twitter/bootstrap
 //= require_tree.
 //= require ckeditor/init
+//= require welcome.js
---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("div.span2 a").fancybox({
+		'transitionIn'	: 'elastic',
+		'transitionOut'	: 'elastic',
+		'autoScale' : false,
+		'autoDimensions': false
+	});
+});

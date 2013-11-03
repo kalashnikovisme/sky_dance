88dfd7e4d9e1b48bcd5b0fd1d0db7904632e51d8
pavel <kalashnikovisme@gmail.com>
2013-11-03 13:46:43 +0400

    add working search input

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,5 +10,4 @@ SkyDance::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.assets.compress = false
   config.assets.debug = true
-  config.assets.precompile = false
 end

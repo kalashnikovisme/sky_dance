a86fa5d8c1b977c93abcbdccf76fe6d4603de342
pavel <kalashnikovisme@gmail.com>
2013-11-05 03:10:11 +0400

    fix helpers

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,9 +2,11 @@ module WelcomeHelper
   include AutoHtml
 
   def office_on_google_map
-    office_on_google_maps_link = OfficeMap.first.link
-    auto_html office_on_google_maps_link do
-      google_map link_text: I18n.t('.view_map')
+    if OfficeMap.any?
+      office_on_google_maps_link = OfficeMap.first.link
+      auto_html office_on_google_maps_link do
+        google_map link_text: I18n.t('.view_map')
+      end
     end
   end
 end

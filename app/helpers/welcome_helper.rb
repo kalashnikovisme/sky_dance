f9e07ee1997d15e32629961200835f2af7ab51a5
pavel <kalashnikovisme@gmail.com>
2013-11-05 01:41:20 +0400

    add button to change office_map

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,7 +2,7 @@ module WelcomeHelper
   include AutoHtml
 
   def office_on_google_map
-    office_on_google_maps_link = OfficeMap.first
+    office_on_google_maps_link = OfficeMap.first.link
     auto_html office_on_google_maps_link do
       google_map link_text: I18n.t('.view_map')
     end

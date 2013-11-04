5371bf6238c770093a17278cc4ffd15f09cf1a39
pavel <kalashnikovisme@gmail.com>
2013-11-04 23:14:44 +0400

    add google_maps

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,9 +1,10 @@
 module WelcomeHelper
   include AutoHtml
 
-  def google_map(link)
-    auto_html link do
-      google_map
+  def office_on_google_map
+    office_on_google_maps_link = "https://maps.google.ru/maps?q=ульяновск+2+переулок+мира+24"
+    auto_html office_on_google_maps_link do
+      google_map link_text: I18n.t('.view_map')
     end
   end
 end
